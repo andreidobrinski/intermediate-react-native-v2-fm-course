4db6a8931a93071f0b83b6c32070d7c5f979757c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-09-25 20:31:05 -0400

    add plant card array

--- plantly/app/(tabs)/index.tsx
+++ plantly/app/(tabs)/index.tsx
@@ -1,12 +1,28 @@
+import { PlantCard } from "@/components/PlantCard";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { usePlantStore } from "@/store/plantsStore";
+import { useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
+  const router = useRouter();
+  const plants = usePlantStore((state) => state.plants);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={plants}
+      renderItem={({ item }) => <PlantCard plant={item} />}
+      keyExtractor={(item) => item.id}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      ListEmptyComponent={
+        <PlantlyButton
+          title="Add your first plant"
+          onPress={() => router.navigate("/new")}
+        />
+      }
+    />
   );
 }
 
@@ -14,7 +30,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  contentContainer: {
+    padding: 12,
   },
 });
