5bc56b52446d149e65fcc4d5b38fd7259bef5517
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-09-26 21:04:56 -0400

    add quick action

--- plantly/app/_layout.tsx
+++ plantly/app/_layout.tsx
@@ -1,10 +1,26 @@
 import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
 
 export default function RootLayout() {
   // use the SplashScreen methods for keeping the splash screen up and hiding it
   // good pattern for auth to keep the screen open while an async function runs
   // SplashScreen.preventAutoHideAsync();
   // SplashScreen.hideAsync();
+  useQuickActionRouting();
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add a plant",
+        icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+        id: "0",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen
