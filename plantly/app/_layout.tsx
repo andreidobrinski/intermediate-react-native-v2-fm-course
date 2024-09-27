de90c6fdea1ee45596ebaa56548848c08fdfe9a4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-09-26 20:42:57 -0400

    splash screen notes

--- plantly/app/_layout.tsx
+++ plantly/app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 
 export default function RootLayout() {
+  // use the SplashScreen methods for keeping the splash screen up and hiding it
+  // good pattern for auth to keep the screen open while an async function runs
+  // SplashScreen.preventAutoHideAsync();
+  // SplashScreen.hideAsync();
   return (
     <Stack>
       <Stack.Screen
